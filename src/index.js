0b5856ce6e3e436864869f8fbebe19eee6ae55f4
Микола Іванишин <43409421+Mikaeri@users.noreply.github.com>
2023-04-22 20:59:59 +0100

    update

--- src/index.js
+++ src/index.js
@@ -5,6 +5,4 @@ import { onSearchBox } from './js/onSearchBox';
 const DEBOUNCE_DELAY = 300;
 const debounce = require('lodash.debounce');
 
-refs.searchBox.addEventListener('input',debounce(onSearchBox, DEBOUNCE_DELAY));
-
-
+refs.searchBox.addEventListener('input', debounce(onSearchBox, DEBOUNCE_DELAY));
--- src/js/onSearchBox.js
+++ src/js/onSearchBox.js
@@ -1,35 +1,35 @@
 import refs from './refs';
-import {fetchCountries} from './fetchCountries'
-import {listMarkup} from './listMarkup';
+import { fetchCountries } from './fetchCountries';
+import { listMarkup } from './listMarkup';
 import { countryInfoMarkup } from './countryInfoMarkup';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 export function onSearchBox(evt) {
-    const searchQuery = refs.searchBox.value;
-    fetchCountries(searchQuery.trim())
-      .then((data) => {
-        refs.countriesList.innerHTML = '';
+  const searchQuery = refs.searchBox.value;
+  fetchCountries(searchQuery.trim())
+    .then(data => {
+      refs.countriesList.innerHTML = '';
 
-    if (data.length === 1){
-      const countriesMarkup = countryInfoMarkup(data);
-      refs.countriesList.insertAdjacentHTML('beforeend', countriesMarkup);
-    }
-    else if(data.length > 1 && data.length <= 10){
+      if (data.length === 1) {
+        const countriesMarkup = countryInfoMarkup(data);
+        refs.countriesList.insertAdjacentHTML('beforeend', countriesMarkup);
+      } else if (data.length > 1 && data.length <= 10) {
         const countriesMarkup = listMarkup(data);
         refs.countriesList.insertAdjacentHTML('beforeend', countriesMarkup);
-    }
-    else {
+      } else {
         refs.countriesList.innerHTML = '';
-        Notify.warning('Too many matches found. Please enter a more specific name.');
-    }
-      })
-      .catch((err) => {
-        refs.countriesList.innerHTML = '';
-        if (searchQuery.trim() ===""){
-            return;
-        }
-      else if (err.message ==="404"){
-          return Notify.failure('Oops, there is no country with that name');
-        }
-  })
+        Notify.warning(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      }
+    })
+    .catch(err => {
+      refs.countriesList.innerHTML = '';
+      if (searchQuery.trim() === '') {
+        return;
+      } else if (err.message === '404') {
+        console.log(err.message);
+        return Notify.failure('Oops, there is no country with that name');
+      }
+    });
 }
